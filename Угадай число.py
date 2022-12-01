3e2d95b35dc4d08387af71fb636081c5321939e6
Timur Stepanov <77236795+Timur-SA@users.noreply.github.com>
2022-12-01 17:57:32 +0300

    Доработал логику игры и добавил цветной текст

--- Угадай число.py
+++ Угадай число.py
@@ -1,39 +1,44 @@
 import random as r
 from loguru import logger 
+from rich.console import Console
 import os
 
 #Переменные
+console = Console()
 run = True #Отвечает за продолжение цикла
-awr = "Утка" #Отвечает за выход из игры
+awr = "Текст" #Отвечает за выход из игры
 AI_number = 0
 player_number = 0
 
 
 #MAIN LOOP
-while run: 
+while True: 
 	
+	run = True
 	print("Я загадал число от 1 до 100, попробуйте его угадать!")
 	print("=" * 40)
 	AI_number = r.randint(1, 100)
 	logger.debug(AI_number)
 	
 	#Игра
-	while True:
+	while run:
 		player_number = int(input("Введите своё число: "))
-		if player_number == AI_number:
-			print("Вы угадали!")
-			
-			if input("Хотите сыграть ещё раз? (Да/Нет): ") == "Д" or "Y" or "Да" or "Yes":
-				break
-			else:
+		if player_number == AI_number:			
+			run = False
+			console.print("Вы угадали!", style="bold green")
+			print("=" * 40)
+			awr = input('Хотите сыграть ещё раз? Введите "0" если нет: ')
+			if awr == "0":
 				quit()
+			else:
+				pass
 		elif player_number > AI_number:
-			print("Моё число меньше вашего. Берите меньше")
+			console.print("Моё число [bold cyan]меньше[/bold cyan] вашего. Берите меньше")
 		else:
-			print("Моё число больше вашего. Берите больше")
+			console.print("Моё число [bold cyan]больше[/bold cyan] вашего. Берите больше")
 		print("=" * 40)
 
-	os.console("cls")
+	os.system('cls')
 
 input("Выйти")
 quit()
